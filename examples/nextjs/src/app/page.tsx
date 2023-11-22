8122e775dad92ae539ad37c95e32f3100d9bd0ea
Ivica Batinić <ivicabatinic123@gmail.com>
2023-11-22 09:34:43 +0100

    Add new illustrations and update page layout

--- examples/nextjs/src/app/page.tsx
+++ examples/nextjs/src/app/page.tsx
@@ -1,9 +1,12 @@
 'use client'
-import { Grid, GridItem, Text, useClipboard, useToast } from "@chakra-ui/react";
+import { Grid, GridItem, Heading, Text, useClipboard, useToast } from "@chakra-ui/react";
 
 import { icons } from "../../.ikona/icons";
+import { illustrations } from "../../.ikona/illustrations";
 import { Icon } from "@/components/media-and-icons/icon/Icon";
 import { IconName } from "../../.ikona/types/icon-name";
+import { IllustrationPath } from "../../.ikona/types/illustration-path";
+import { Illustration } from "@/components/media-and-icons/illustration/Illustration";
 
 const IconPreview = ({ name }: { name: IconName } ) => {
   const toast = useToast();
@@ -11,7 +14,6 @@ const IconPreview = ({ name }: { name: IconName } ) => {
 
   return (
     <GridItem
-      key={name}
       sx={{ '& > svg': { boxSize: '30px' } }}
       alignItems="center"
       flexDir="column"
@@ -43,12 +45,58 @@ const IconPreview = ({ name }: { name: IconName } ) => {
   );
 }
 
+const IllustrationPreview = ({ src }: { src: IllustrationPath } ) => {
+  const toast = useToast();
+  const { onCopy } = useClipboard(`<Illustration path="${src}" />`);
+
+  return (
+    <GridItem
+      sx={{ '& > svg': { boxSize: '30px' } }}
+      alignItems="center"
+      flexDir="column"
+      display="flex"
+      p={2}
+      bg="whiteAlpha.400"
+      borderRadius="md"
+      shadow="base"
+      _hover={{
+        bg: 'whiteAlpha.500',
+      }}
+      cursor="pointer"
+      colSpan={1}
+      onClick={() => {
+        onCopy();
+        toast({
+          title: `Copied "<Illustration src="${src}" />" to clipboard!`,
+          status: 'success',
+          duration: 2000,
+          isClosable: true,
+        });
+      }}
+    >
+      <Illustration src={src} />
+      <Text mt={2} fontSize="xx-small" textAlign="center">
+        {src}
+      </Text>
+    </GridItem>
+  );
+}
+
 export default function Home() {
   return (
-    <Grid gap={4} templateColumns="repeat(5, 1fr)" pb={6} color="black" _dark={{ color: 'white' }}>
-      {icons.map((name) => (
-        <IconPreview name={name} key={name} />
-      ))}
-    </Grid>
+    <>
+      <Heading>Icons:</Heading>
+      <Grid gap={4} templateColumns="repeat(5, 1fr)" pb={6} color="black" _dark={{ color: 'white' }}>
+        {icons.map((name) => (
+          <IconPreview name={name} key={name} />
+        ))}
+      </Grid>
+      <Heading>Illustrations:</Heading>
+      <Grid gap={4} templateColumns="repeat(5, 1fr)" pb={6} color="black" _dark={{ color: 'white' }}>
+        {illustrations.map((src) => (
+          <IllustrationPreview src={src} key={src} />
+        ))}
+      </Grid>
+    </>
   )
 }
