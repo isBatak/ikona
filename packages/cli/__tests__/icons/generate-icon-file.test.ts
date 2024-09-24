77f01503a069f6037a1feff8666c1d0c114b10f4
Ivica Batinić <ivicabatinic123@gmail.com>
2024-09-24 14:20:43 +0200

    chore: fix snapshot

--- packages/cli/__tests__/icons/generate-icon-file.test.ts
+++ packages/cli/__tests__/icons/generate-icon-file.test.ts
@@ -70,7 +70,9 @@ describe("generateIconFiles", () => {
 
     const hashContent = fs.readFileSync(context.hashPath, "utf-8");
     expect(hashContent).toMatchInlineSnapshot(`
-      "export const hash = '`);
+      "export const hash = 'e50b086ece17b1aef95e165676c128d0';
+      "
+    `);
   });
 
   it("should generate and optimize sprite", async () => {
